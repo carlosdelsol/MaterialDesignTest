8dd3655a890a9a11142d23f51f70b4caa7737fc0
Carlos del Sol <carlosdelsol@gmail.com>
2015-11-09 23:35:41 +0100

    3 Themes, Colors, Typography, Layout and Animation

---
+++ app/src/main/java/soft/academy/materialdesigntest/TileContentFragment.java
@@ -0,0 +1,17 @@
+package soft.academy.materialdesigntest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+/**
+ * Created by carlosdelsol on 9/11/15.
+ */
+public class TileContentFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.item_tile, null);
+    }
+}
